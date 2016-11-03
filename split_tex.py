89033c077de9f21e707870b1b725af459f98ed19
Philipp Dufter <philipp.dufter@web.de>
2016-11-03 17:32:15 +0100

    first try

--- split_tex.py
+++ split_tex.py
@@ -1,7 +1,26 @@
-
+import re
+import os
 
 # parse file
 with open('example.tex') as f:
-    # todo 
+    # TODO improve parsing
+    # TODO test enocding
     data=f.read().replace('\n', '')
-    print data
+
+# get document 
+document = re.split(r"\{document\}", data)[1]
+
+
+sections = re.split(r"\\section\{(.+?)\}", data)[1:]
+
+
+directory = "content"
+if not os.path.exists(directory):
+    os.makedirs(directory)
+else:
+    raise ValueError("Directory 'content' exists already.")
+
+for i in range(0, len(sections) / 2):
+    with open(directory + "/" + sections[2 * i] + ".tex", "w") as f:
+        # TODO formatting is broke obviously
+        f.write("\section{" + sections[2 * i] + "}" + sections[2 * i + 1])
